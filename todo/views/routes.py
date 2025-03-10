6089ea9d474a90da1b93d277902d548bee98955d
oiiDawn <oii.myself@outlook.com>
2025-03-10 13:57:12 +1000

    Prac2

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, jsonify
- 
-api = Blueprint('api', __name__, url_prefix='/api/v1') 
+from flask import Blueprint, jsonify, request
+from todo.models import db
+from todo.models.todo import Todo
+from datetime import datetime, timedelta
+
+api = Blueprint('api', __name__, url_prefix='/api/v1')
 
 TEST_ITEM = {
     "id": 1,
@@ -11,8 +14,9 @@ TEST_ITEM = {
     "created_at": "2023-02-20T00:00:00",
     "updated_at": "2023-02-20T00:00:00"
 }
- 
-@api.route('/health') 
+
+
+@api.route('/health')
 def health():
     """Return a status of 'ok' if the server is running and listening to request"""
     return jsonify({"status": "ok"})
@@ -21,25 +25,82 @@ def health():
 @api.route('/todos', methods=['GET'])
 def get_todos():
     """Return the list of todo items"""
-    return jsonify([TEST_ITEM])
+    completed = request.args.get("completed") == "true" if request.args.get("completed") else None
+    window = int(request.args.get("window")) if request.args.get("window") else None
+    todos = db.session.query(Todo)
+    if completed is not None:
+        todos = todos.filter(Todo.completed == completed)
+    if window:
+        now = datetime.utcnow().date()
+        days_later = now + timedelta(days=window)
+        todos = todos.filter(Todo.deadline_at <= days_later)
+    todos = todos.all()
+    result = [todo.to_dict() for todo in todos]
+    return jsonify(result)
+
 
 @api.route('/todos/<int:todo_id>', methods=['GET'])
 def get_todo(todo_id):
     """Return the details of a todo item"""
-    return jsonify(TEST_ITEM)
+    todo = db.session.get(Todo, todo_id)
+    if todo is None:
+        return jsonify({"error": "Todo not found"}), 404
+    return jsonify(todo.to_dict())
+
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
     """Create a new todo item and return the created item"""
-    return jsonify(TEST_ITEM), 201
+    valid_keys = {"title", "description", "completed", "deadline_at"}
+    if request.json.get("title") is None:
+        return jsonify({"error": "missing title"}), 400
+    if set(request.json.keys()) - valid_keys:
+        return jsonify({"error": "extra keys"}), 400
+    
+    todo = Todo(
+        title=request.json.get("title"),
+        description=request.json.get("description"),
+        completed=request.json.get("completed", False),
+    )
+    if "deadline_at" in request.json:
+        todo.deadline_at = datetime.fromisoformat(request.json.get("deadline_at"))
+    
+    # Adds a new record to the database or will update an existing record.
+    db.session.add(todo)
+    # Commits the changes to the database.
+    # This must be called for the changes to be saved.
+    db.session.commit()
+    return jsonify(todo.to_dict()), 201
+
 
 @api.route('/todos/<int:todo_id>', methods=['PUT'])
 def update_todo(todo_id):
     """Update a todo item and return the updated item"""
-    return jsonify(TEST_ITEM)
+    todo = db.session.get(Todo, todo_id)
+    if todo is None:
+        return jsonify({"error": "Todo not found"}), 404
+    if "id" in request.json:
+        return jsonify({"error": "cannot change id"}), 400
+    valid_keys = {"title", "description", "completed", "deadline_at"}
+    if set(request.json.keys()) - valid_keys:
+        return jsonify({"error": "extra keys"}), 400
+    
+    todo.title = request.json.get("title", todo.title)
+    todo.description = request.json.get("description", todo.description)
+    todo.completed = request.json.get("completed", todo.completed)
+    todo.deadline_at = request.json.get("deadline_at", todo.deadline_at)
+    db.session.commit()
+    
+    return jsonify(todo.to_dict())
+
 
 @api.route('/todos/<int:todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
     """Delete a todo item and return the deleted item"""
-    return jsonify(TEST_ITEM)
- 
+    todo = db.session.get(Todo, todo_id)
+    if todo is None:
+        return jsonify({}), 200
+    
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify(todo.to_dict()), 200
